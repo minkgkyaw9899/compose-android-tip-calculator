476a024ce133e2f5d74d5ee54121bc0c646cd349
Julian Kaung <minkgkyaw9899@gmail.com>
2025-10-04 14:36:15 +0630

    feat: learned state with input

--- app/src/main/java/com/mkk/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/mkk/tipcalculator/MainActivity.kt
@@ -8,17 +8,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mkk.tipcalculator.ui.theme.TipCalculatorTheme
@@ -31,9 +42,11 @@ class MainActivity : ComponentActivity() {
     setContent {
       TipCalculatorTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Surface(modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize()) {
+          Surface(
+            modifier = Modifier
+              .padding(innerPadding)
+              .fillMaxSize()
+          ) {
             TipTimeLayout()
           }
         }
@@ -44,10 +57,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+  var amountInput by remember {
+    mutableStateOf("0")
+  }
+  val amount = amountInput.toDoubleOrNull() ?: 0.0
+  val tip = calculateTip(amount)
+
   Column(
     modifier = Modifier
       .statusBarsPadding()
-      .padding(horizontal = 40.dp),
+      .padding(horizontal = 40.dp)
+      .verticalScroll(rememberScrollState())
+      .safeDrawingPadding(),
     horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Center
   ) {
@@ -59,12 +80,42 @@ fun TipTimeLayout() {
         )
         .align(alignment = Alignment.Start)
     )
+    EditNumberField(
+      amountInput = amountInput,
+      onValueChange = { amountInput = it },
+      modifier = Modifier
+    )
     Text(
-      text = stringResource(R.string.tip_amount, "$0.00"),
+      text = stringResource(R.string.tip_amount, tip),
       style = MaterialTheme.typography.displaySmall
     )
     Spacer(modifier = Modifier.height(150.dp))
   }
+}
+
+@Composable
+fun EditNumberField(
+  modifier: Modifier = Modifier,
+  amountInput: String,
+  onValueChange: (String) -> Unit
+) {
+  TextField(
+    value = amountInput,
+    onValueChange = onValueChange,
+    label = {
+      Text(
+        text = stringResource(R.string.bill_amount)
+      )
+    },
+    singleLine = true,
+    keyboardOptions = KeyboardOptions(
+      keyboardType = KeyboardType.Number
+    ),
+    modifier = modifier
+      .padding(bottom = 32.dp)
+      .fillMaxWidth()
+
+  )
 }
 
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
@@ -72,7 +123,7 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
   return NumberFormat.getCurrencyInstance().format(tip)
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
   TipCalculatorTheme {
